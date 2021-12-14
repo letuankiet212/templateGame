1e10953269a83a6cd50623502442867bb128d9da
letuankiet212 <48205415+letuankiet212@users.noreply.github.com>
2021-12-14 22:27:21 +0700

    up

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function () {
+  //Navbar right
+  $('.i-control').click(function () {
+    $('.anchor').toggleClass('open');
+    $(this).toggleClass('i-control-open'); // active bg btn
+  });
+
+  /* customJS */
+});
